b7e22e0d87f020e5f6bd52bd33f3ec3a925d6fa6
Atilla YAS <atilla.yas@gmail.com>
2024-10-20 22:25:55 +0200

    dataProvider

---
+++ src/Entity/Dependency.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+#[ApiResource(
+    collectionOperations: ['get'],
+    itemOperations: ['get'],
+    paginationEnabled: false
+)]
+class Dependency
+{
+    #[ApiProperty(
+        identifier: true
+    )]
+    private string $uuid;
+    #[ApiProperty(
+        description: 'Nom de la dependance'
+    )]
+    private string $name;
+    #[ApiProperty(
+        description: 'Version de la dependance',
+        openapiContext: [
+            'example' => "5.4.*"
+        ]
+    )]
+    private string $version;
+    public function __construct(string $uuid, string $name, string $version)
+    {
+         $this->uuid = $uuid;
+         $this->name = $name;
+         $this->version = $version;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+}
