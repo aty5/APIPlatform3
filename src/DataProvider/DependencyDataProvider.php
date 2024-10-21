fc102fd41ccc19b5c051cdf87f03901bb47746ae
Atilla YAS <atilla.yas@gmail.com>
2024-10-21 02:05:13 +0200

    dataPersister

--- src/DataProvider/DependencyDataProvider.php
+++ src/DataProvider/DependencyDataProvider.php
@@ -6,34 +6,14 @@ use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\Dependency;
+use App\Repository\DependencyRepository;
 use Ramsey\Uuid\Uuid;
 
 class DependencyDataProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface, ItemDataProviderInterface
 {
 
-    public function __construct(private string $rootPath)
+    public function __construct(private DependencyRepository $repository)
     {
-    }
-
-    private function getDependencies():array
-    {
-        $path = $this->rootPath . '/composer.json';
-
-        $json = json_decode(file_get_contents($path), true);
-
-        return $json['require'];
-    }
-    public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
-    {
-        $path = $this->rootPath . '/composer.json';
-
-        $json = json_decode(file_get_contents($path), true);
-
-        $items = [];
-        foreach ($this->getDependencies() as $name => $version){
-            $items[] = new Dependency(Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString(), $name, $version);
-        }
-        return $items;
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
@@ -43,14 +23,11 @@ class DependencyDataProvider implements ContextAwareCollectionDataProviderInterf
 
     public function getItem(string $resourceClass, $id, string $operationName = null, array $context = [])
     {
-        $dependencies = $this->getDependencies();
-        foreach ($dependencies as $name => $version){
-            $uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();
-            if ($uuid == $id)
-            {
-                return new Dependency($uuid, $name, $version);
-            }
-        }
-        return null;
+        return $this->repository->find($id);
+    }
+
+    public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
+    {
+        return $this->repository->findAll();
     }
 }
--- src/Entity/Dependency.php
+++ src/Entity/Dependency.php
@@ -4,10 +4,24 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
-    collectionOperations: ['get'],
-    itemOperations: ['get'],
+    collectionOperations: ['get', 'post'],
+    itemOperations: [
+        'get',
+        'put' => [
+            'denormalization_context' => [
+                'groups' => ['put:Dependency']
+            ]
+        ],
+        'delete' => [
+            ''
+        ]
+    ],
     paginationEnabled: false
 )]
 class Dependency
@@ -18,7 +32,8 @@ class Dependency
     private string $uuid;
     #[ApiProperty(
         description: 'Nom de la dependance'
-    )]
+    ),
+    Length(min: 6), NotBlank()]
     private string $name;
     #[ApiProperty(
         description: 'Version de la dependance',
@@ -26,12 +41,21 @@ class Dependency
             'example' => "5.4.*"
         ]
     )]
+    #[Length(min: 6), NotBlank(), Groups(['put:Dependency'])]
     private string $version;
-    public function __construct(string $uuid, string $name, string $version)
+    public function __construct(string $name, string $version)
     {
-         $this->uuid = $uuid;
+         $this->uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();
          $this->name = $name;
          $this->version = $version;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
     }
 
     /**
@@ -48,6 +72,14 @@ class Dependency
     public function getName(): string
     {
         return $this->name;
+    }
+
+    /**
+     * @param string $version
+     */
+    public function setVersion(string $version): void
+    {
+        $this->version = $version;
     }
 
     /**
